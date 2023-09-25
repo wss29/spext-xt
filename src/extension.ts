165f2125d347cedd2120ca3dbbc467a9cce33b3b
qingtingzhiyu <return_go@163.com>
2023-09-25 15:58:40 +0800

    ww

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// Import the module and referencedd it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // This method is called whesssn your extension is activated
